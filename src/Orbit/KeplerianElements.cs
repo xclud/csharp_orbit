250de1f942aa6a9f8ff9851a487e642df4d804d1
Mahdi K. Fard <mkfard@outlook.com>
2022-06-24 23:03:03 +0430

    Last second fix for KeplerianElements.

--- src/Orbit/KeplerianElements.cs
+++ src/Orbit/KeplerianElements.cs
@@ -2,7 +2,7 @@
 
 namespace System;
 
-public sealed class KeplerianElements<T> where T : INumber<T>, IFloatingPoint<T>
+public sealed class KeplerianElements<T> : IKeplerianElements<T> where T : INumber<T>, IFloatingPoint<T>
 {
     public KeplerianElements(T epoch, T eccentricity, T inclination, T rightAscensionOfAscendingNode, T argumentOfPeriapsis, T meanMotion, T meanAnomaly, T drag, int revolutionNumberAtEpoch)
     {
@@ -34,4 +34,22 @@ public sealed class KeplerianElements<T> where T : INumber<T>, IFloatingPoint<T>
     public readonly T Drag;
 
     public readonly int RevolutionNumberAtEpoch;
+
+    T IKeplerianElements<T>.Epoch => Epoch;
+
+    T IKeplerianElements<T>.Eccentricity => Eccentricity;
+
+    T IKeplerianElements<T>.MeanMotion => MeanMotion;
+
+    int IKeplerianElements<T>.RevolutionNumberAtEpoch => RevolutionNumberAtEpoch;
+
+    T IKeplerianElements<T>.Inclination => Inclination;
+
+    T IKeplerianElements<T>.RightAscensionOfAscendingNode => RightAscensionOfAscendingNode;
+
+    T IKeplerianElements<T>.MeanAnomaly => MeanAnomaly;
+
+    T IKeplerianElements<T>.ArgumentOfPeriapsis => ArgumentOfPeriapsis;
+
+    T IKeplerianElements<T>.Drag => Drag;
 }
