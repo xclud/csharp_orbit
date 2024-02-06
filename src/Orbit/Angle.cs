8d271942f7e5b80064652192a131d93bf700a22a
Mahdi <mkfard@outlook.com>
2024-02-07 03:05:05 +0330

    Adds Angle class.

---
+++ src/Orbit/Angle.cs
@@ -0,0 +1,27 @@
+﻿using System.Numerics;
+
+namespace System.Astronomy;
+
+public sealed record Angle<T> where T : INumber<T>, IFloatingPoint<T>
+{
+    public readonly T Degrees;
+    public readonly T Radians;
+
+    private static readonly T _180 = T.Parse("180", null);
+
+    private Angle(T degrees, T radians)
+    {
+        Degrees = degrees;
+        Radians = radians;
+    }
+
+    public static Angle<T> FromDegrees(T degrees)
+    {
+        return new Angle<T>(degrees, degrees / _180 * T.Pi);
+    }
+
+    public static Angle<T> FromRadians(T radians)
+    {
+        return new Angle<T>(radians * _180 / T.Pi, radians);
+    }
+}
